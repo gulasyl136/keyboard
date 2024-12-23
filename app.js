040651ddf926abb22ad6a533eb715d92b688175d
gulasyl136 <muhtarovagulasyl@gmail.com>
2024-12-23 21:11:29 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const capsLock = document.getElementById("caps-lock");
+	capsLock?.addEventListener("click", function () {
+		const pressed = this.ariaPressed === "true";
+		this.setAttribute("aria-pressed", !pressed);
+	});
+});
